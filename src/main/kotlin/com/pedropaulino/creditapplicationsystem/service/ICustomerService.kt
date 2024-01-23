bd8f7463df202cea43c04f494c434bc5252d6f02
Pedro Paulino <pedrogpaulino@gmail.com>
2024-01-23 18:02:32 -0300

    feat: Implementação dos métodos no customer

--- src/main/kotlin/com/pedropaulino/creditapplicationsystem/service/ICustomerService.kt
+++ src/main/kotlin/com/pedropaulino/creditapplicationsystem/service/ICustomerService.kt
@@ -2,11 +2,11 @@ package com.pedropaulino.creditapplicationsystem.service
 
 import com.pedropaulino.creditapplicationsystem.entity.Customer
 
-interface ICustomerInterface {
+interface ICustomerService {
 
     fun save(customer: Customer): Customer
 
     fun findById(customerID: Long): Customer
 
-    fun delete(customerID: Long): Customer
+    fun delete(customerID: Long)
 }
