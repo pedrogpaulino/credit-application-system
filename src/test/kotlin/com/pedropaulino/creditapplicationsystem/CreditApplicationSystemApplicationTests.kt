b9d3766f84f6088f69314e04b8ee7efdcabd3009
Pedro Paulino <pedrogpaulino@gmail.com>
2024-01-23 14:50:51 -0300

    Inicial commit

---
+++ src/test/kotlin/com/pedropaulino/creditapplicationsystem/CreditApplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pedropaulino.creditapplicationsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
