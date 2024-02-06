8f99e3adc553a605a689c05d0ba995e0a77c7c4a
Pedro Paulino <pedrogpaulino@gmail.com>
2024-02-05 21:30:31 -0300

    feat: add customer updateById

--- src/main/kotlin/com/pedropaulino/creditapplicationsystem/controller/CustomerController.kt
+++ src/main/kotlin/com/pedropaulino/creditapplicationsystem/controller/CustomerController.kt
@@ -1,15 +1,19 @@
 package com.pedropaulino.creditapplicationsystem.controller
 
 import com.pedropaulino.creditapplicationsystem.dto.CustomerDto
+import com.pedropaulino.creditapplicationsystem.dto.CustomerUpdateDto
 import com.pedropaulino.creditapplicationsystem.dto.CustomerView
 import com.pedropaulino.creditapplicationsystem.entity.Customer
 import com.pedropaulino.creditapplicationsystem.service.impl.CustomerService
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -34,5 +38,17 @@ class CustomerController (
 
     @DeleteMapping("/id")
     fun deleteById(@PathVariable id: Long) = this.customerService.delete(id)
+
+    @PatchMapping
+    fun updateById(@RequestParam(value = "customerId") id: Long,
+                   @RequestBody customerUpdateDto: CustomerUpdateDto): CustomerView{
+
+        val customer: Customer= this.customerService.findById(id)
+        val customerToUpdate = customerUpdateDto.toEntity(customer)
+
+        val customerUpdated: Customer = this.customerService.save(customerToUpdate)
+
+        return CustomerView(customerUpdated)
+    }
 
 }
---
+++ src/main/kotlin/com/pedropaulino/creditapplicationsystem/dto/CustomerUpdateDto.kt
@@ -0,0 +1,22 @@
+package com.pedropaulino.creditapplicationsystem.dto
+
+import com.pedropaulino.creditapplicationsystem.entity.Customer
+import java.math.BigDecimal
+
+data class CustomerUpdateDto(
+        val firstName: String,
+        val lastName: String,
+        val income: BigDecimal,
+        val zipCode: String,
+        val street: String,
+) {
+    fun toEntity(customer: Customer): Customer{
+        customer.firstName = this.firstName
+        customer.lastName = this.lastName
+        customer.income = this.income
+        customer.address.zipCode = this.zipCode
+        customer.address.street= this.street
+
+        return customer
+    }
+}
